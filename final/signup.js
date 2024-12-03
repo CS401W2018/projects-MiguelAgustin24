dc264e0fa18663ef6ebb17cf12a88e06531e23bb
MiguelAgustin24 <agustinmiguel0316@gmail.com>
2024-12-03 04:02:39 -1000

    add changes

--- final/signup.js
+++ final/signup.js
@@ -5,34 +5,33 @@ document.getElementById('myForm').addEventListener('submit', function(event){
     const age = document.getElementById('age').value;
     const newemail = document.getElementById('new-email').value;
 
-    if(!first || !last){
+    if (!first || !last) {
         alert('Please fill out your First and Last name');
         return;
     }
-    if(!newemail){
+    if (!newemail) {
         alert('Please fill out your email address');
         return;
     }
-    if(!age|| age<12){
+    if (!age || age < 12) {
         alert('You must be accompanied by an adult to submit this form');
         return;
     }
+
     const formData = {
         first: first,
         last: last,
         age: age,
-        Email: newemail
+        email: newemail
     };
 
     const xhr = new XMLHttpRequest();
-    xhr.open("GET","signup.json", true);
+    xhr.open("POST", "process.html", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4 && xhr.status === 200){
-           const response = JSON.parse(xhr.responseText);
-                document.getElementById("message").innerHTML = response.message;
-                document.getElementById('myForm').innerHTML = " ";
-        } else if(xhr.readyState === 4 ){
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            window.location.href = "process.html";
+        } else if (xhr.readyState === 4) {
             alert("Error: " + xhr.responseText);
         }
     };
